cdca076ec6ce86c723b6336ac0e1c5e7ca5a2454
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-11-08 21:35:02 +0100

    Added useEffect for fetching projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+import { response } from 'express';
+interface Projects {
+  boards: []
+  _id: string,
+  name: String,
+  columns: [{
+    name: String,
+    tasks: [{
+      title: String,
+      description: String,
+      status: String,
+      subtasks: [{
+        title: String,
+        isCompleted: Boolean
+      }]
+    }],
+  }]
 
+
+}
 function App() {
+  const [projects, setProjects] = useState<Projects[]>([])
+
+  useEffect(() => {
+    axios.get('https://kanban-task-server-7zl1.onrender.com/projects')
+      .then(response => {
+        setProjects(response.data[0].boards || [])
+      })
+  })
   return (
     <div className="App">
-      KANBAN
+      {projects && projects.map((project) => {
+        return <div>
+          <p>{project?.name}</p>
+        </div>
+      })}
     </div>
   );
 }
