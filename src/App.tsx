5d9aaa3bfd377e062fbf401e2602caae4cd7028f
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-11-09 22:48:35 +0100

    Wrote function for displaying selected project name

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,14 @@ interface Projects {
   }]
 }
 
+interface ProjectData {
+  projectName: string 
+}
 function App() {
   const [projects, setProjects] = useState<Projects[]>([])
+  const [projectData, setProjectData] = useState<ProjectData>({
+    projectName: "Platform Launch"
+  })
 
   const onChange = (checked: boolean) => {
     console.log(`switch to ${checked}`);
@@ -38,37 +44,59 @@ function App() {
       })
   }, [])
 
+  const handleSelectedProject = (selectedProjectName:any) => {
+    if (projects) {
+      const projectName = projects.find(project => selectedProjectName === project.name)
+      setProjectData({...projectData, projectName: projectName?.name || ""})
+    }
+  }
+
   useEffect(() => {
-    // return projects ? console.log(projects[0].columns.map(column=> column.name)) : console.log("Fetching")
-    // return projects ? console.log(projects.map(project=> project.name)) : console.log("Fetching")
-    //return projects ? console.log(projects.length) : console.log("Fetching")
-  }, [projects])
+   setProjectData({...projectData, projectName:"Platform Launch"})
+  }, [])
 
   return (
-    <div className="App">
+    <div className="container">
       <div className='sidebar'>
         <div className='nav-list'>
-          <img src="/images/logo-light.svg" alt="logo" />
-          <p>{`All boards (${projects.length})`}</p>
+          <div className="logo">
+            <img src="/images/logo-light.svg" alt="logo" />
+          </div>
 
-          {projects &&
-            projects.map((project) => {
-              return <div key={project._id} className='boards-list'>
-                <img src="/images/viewIcon.png" alt="view board" />
-                <p>{project.name}</p>
-              </div>
-            })
-          }
-          < div className='boards-list'>
-            <img src="/images/viewIcon.png" alt="view board" />
-            <p>+ Create New Board</p>
+          <div className="all-boards">
+            <p>{`All boards (${projects.length})`}</p>
+            {projects &&
+              projects.map((project) => {
+                return <div key={project._id} onClick={() => handleSelectedProject(project.name)} className='boards-list'>
+                  {/* <a href=""> */}
+                  <img src="/images/viewIcon.png" alt="view board" />
+                  <p>{project.name}</p>
+                  {/* </a> */}
+                </div>
+              })
+            }
+            < div className='new-board'>
+              <img src="/images/viewIcon.png" alt="view board" />
+              <p>+ Create New Board</p>
+            </div>
           </div>
         </div>
         <div className="theme-wrapper">
-          <img src="/images/light-theme.svg" alt="light-theme" />
+          <img src="/images/light-theme.svg" alt="light-theme" className='light' />
           <Switch defaultChecked onChange={onChange} />
-          <img src="/images/dark-theme.svg" alt="dark-theme" />
+          <img src="/images/dark-theme.svg" alt="dark-theme" className='dark' />
         </div>
+      </div>
+      <div className='right-div'>
+        <header className="header">
+          <h1>{projectData.projectName}</h1>
+          <button className='header-btn'>+ Add New Task</button>
+        </header>
+        <main>
+          <div className='status'>
+            <p >{projects && projects.map(project => project.columns.map(column => column.name))}</p>
+          </div>
+        </main>
       </div>
     </div>
   );
